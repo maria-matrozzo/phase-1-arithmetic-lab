f97a7c98448ba780f4fed69605979ef41065c40c
Maria <mmatrozzo@gmail.com>
2021-11-02 09:31:27 -0400

    commit tree d75642f4749aef282b12e18036b19900f55f492d

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+
+let multiply;
+multiply = num1 * num2;
+
+multiply;
+
+let random = 5;
+random;
+
+let num3 = 4;
+let num4 = 0;
+
+let mod;
+mod = num3 + num4;
+
+let max = 20;
+Math.max(20);
